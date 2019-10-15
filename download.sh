1c8bf55d4a4d30b08a761ea500047eda09624c20
Marcos Vieira <marcosbv@br.ibm.com>
2019-10-15 20:19:18 -0300

    Download Script

---
+++ download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ echo '* Downloading script files...'
+
+for i in gatherData.sh policiesForResources.js resourceAccessForUser.js utils.js
+do
+    curl -sL https://raw.githubusercontent.com/marcosbv/ibmcloud-policy-reports/master/$i -o $i
+done
+
+chmod 755 *.sh
+
+if [ $# -gt 0 ]
+then
+     echo '** Gathering data...'
+     ./gatherData.sh $1
+else
+     echo 'Files sucessfully downloaded. Now type ./gatherData.sh '\''<AccountName>'\'' to collect data from account'
+     echo ' NOTE: Before running this command, you must log in in your IBM Cloud account using IBM CLI.'
+fi
