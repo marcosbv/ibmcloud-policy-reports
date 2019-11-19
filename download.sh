47466188a1951d6442369278b9cc329bf4d444e6
Marcos Vieira <marcosbv@br.ibm.com>
2019-11-19 14:35:21 -0300

    Shell script to upload csv reports to COS

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
  echo '* Downloading script files...'
 
-for i in gatherData.sh policiesForResource.js resourceAccessForUser.js utils.js policiesForRole.js latestUserLogin.js
+for i in gatherData.sh policiesForResource.js resourceAccessForUser.js utils.js policiesForRole.js latestUserLogin.js saveAndUploadCSVReports.sh
 do
     curl -sL https://raw.githubusercontent.com/marcosbv/ibmcloud-policy-reports/master/$i -o $i
 done
