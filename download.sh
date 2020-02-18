a3693835aa41ac10b2a58848c1ce42b7bda63f0e
Marcos  Vieira <marcosbv@br.ibm.com>
2020-02-18 16:37:11 -0300

    Include new scripts to download shell script

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
  echo '* Downloading script files...'
 
-for i in gatherData.sh policiesForResource.js resourceAccessForUser.js utils.js policiesForRole.js latestUserLogin.js saveAndUploadCSVReports.sh
+for i in gatherData.sh policiesForResource.js resourceAccessForUser.js utils.js policiesForRole.js latestUserLogin.js saveAndUploadCSVReports.sh policiesForService.js policiesByUserAndAccessGroup.js
 do
     curl -sL https://raw.githubusercontent.com/marcosbv/ibmcloud-policy-reports/master/$i -o $i
 done
